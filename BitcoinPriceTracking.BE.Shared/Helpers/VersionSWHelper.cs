cd3f663437b85f7aa8e7b30fda3872396ada2271
M-Str <miroslav.stransky@live.com>
2025-10-14 23:31:57 +0200

    CC

--- BitcoinPriceTracking.BE.Shared/Helpers/VersionSWHelper.cs
+++ BitcoinPriceTracking.BE.Shared/Helpers/VersionSWHelper.cs
@@ -1,10 +1,5 @@
 ﻿using BitcoinPriceTracking.BE.Shared.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BitcoinPriceTracking.BE.Shared.Helpers
 {
