c4cafbe5083f64a5b33a5f3dee1d96d257bdf5a7
M-Str <miroslav.stransky@live.com>
2025-10-15 01:01:36 +0200

    MOD: přidány servise pro BE část, TimedHosterdservisa

--- BitcoinPriceTracking/Program.cs
+++ BitcoinPriceTracking/Program.cs
@@ -1,13 +1,30 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using BitcoinPriceTracking.Data;
+using BitcoinPriceTracking.BE.Shared.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddHttpClient("ApiClient", (sp, client) =>
+{
+	var httpContextAccessor = sp.GetRequiredService<IHttpContextAccessor>();
+	var request = httpContextAccessor.HttpContext?.Request;
+
+	var baseAddress = request != null ? $"{request.Scheme}://{request.Host.Value}" : "https://localhost:7246/";
+	client.BaseAddress = new Uri(baseAddress);
+});
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddSingleton<IEventLogService, EventLogService>();
+
+
+builder.Services.AddHostedService<TimedHostedService>();
 
 var app = builder.Build();
 
