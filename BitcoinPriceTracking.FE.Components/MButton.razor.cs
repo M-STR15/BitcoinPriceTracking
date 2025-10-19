0a484ae20507eb6d5b0c19f1a2aacd98841ae3e1
M-Str <miroslav.stransky@live.com>
2025-10-19 16:38:08 +0200

    ADD: přidány nové komponenty Button a ModalWindow

---
+++ BitcoinPriceTracking.FE.Components/MButton.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BitcoinPriceTracking.FE.Components
+{
+	public partial class MButton
+	{
+		[Parameter]
+		public string Text { get; set; } = string.Empty;
+
+		[Parameter]
+		public EventCallback OnClick { get; set; }
+
+		[Parameter]
+		public string Src { get; set; }
+
+		[Parameter]
+		public string Style { get; set; } = string.Empty;
+
+		[Parameter]
+		public string Class { get; set; } = string.Empty;
+
+		[Parameter]
+		public bool IsDisabled { get; set; } = false;
+
+		[Parameter]
+		public bool IsVisibleNotification { get; set; } = false;
+
+		[Parameter]
+		public int NumberNotification { get; set; } = 0;
+
+		private string _disabledStyle => IsDisabled ? "pointer-events: none; opacity: 0.5;" : "";
+		[Parameter] 
+		public RenderFragment? ChildContent { get; set; }
+
+		private EventCallback getOnClickCallback()
+		{
+			return IsDisabled ? EventCallback.Empty : OnClick;
+		}
+	}
+}
---
+++ BitcoinPriceTracking/Modals/Models/BaseViewModal.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BitcoinPriceTracking.Modals.Models
+{
+	public abstract class BaseViewModal : ComponentBase
+	{
+		[Inject]
+		protected HttpClient? _httpClient { get; set; }
+
+		[Inject]
+		protected IHttpClientFactory _httpClientFactory { get; set; }
+
+		private bool _visible;
+		[Parameter, EditorRequired]
+		public virtual bool Visible
+		{
+			get => _visible;
+			set
+			{
+				if (_visible != value)
+				{
+					_visible = value;
+					if (VisibleChanged.HasDelegate)
+						VisibleChanged.InvokeAsync(Visible).Wait();
+				}
+			}
+		}
+		[Parameter, EditorRequired]
+		public virtual EventCallback<bool> VisibleChanged { get; set; }
+		[Parameter, EditorRequired]
+		public virtual string Title { get; set; } = string.Empty;
+
+		[Parameter]
+		public virtual EventCallback OnModalClosed { get; set; }  // Událost, kterou spustíme při zavření modálu
+	}
+}
